3d9edc1fd2b4df8840eba61d04d69668de4093f5
David Golodetsky <david.golodetsky@lovecrafts.com>
2022-01-07 22:33:43 +0200

    wip

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,10 +46,7 @@ export default {
     [
       'storyblok-nuxt',
       {
-        accessToken:
-          process.env.NODE_ENV === 'production'
-            ? process.env.STORYBLOK_PUBLIC_TOKEN
-            : process.env.STORYBLOK_PREVIEW_TOKEN,
+        accessToken: process.env.STORYBLOK_PREVIEW_TOKEN,
         cacheProvider: 'memory',
       },
     ],
