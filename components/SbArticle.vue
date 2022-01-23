9e00e54b21e447ffd96c818cc83308ca438d5c4e
David Golodetsky <david.golodetsky@lovecrafts.com>
2022-01-23 18:44:51 +0200

    more images

--- components/SbArticle.vue
+++ components/SbArticle.vue
@@ -2,18 +2,29 @@
   <div v-editable="blok" class="prose lg:prose-xl my-24 mx-auto">
     <h1>{{ blok.name }}</h1>
     <time class="pt-2 pb-4">{{ blok.date }}</time>
-    <SbPhoto
-      :blok="blok.gallery[0].columns[0]"
-      :columns="blok.gallery[0].columns"
-      :show-name="false"
-    />
+    <div class="relative">
+      <SbPhoto
+        :blok="blok.gallery[0].columns[0]"
+        :columns="blok.gallery[0].columns"
+        :show-name="false"
+      />
+      <CarbonImageCopy
+        v-if="blok.gallery[0].columns.length > 1"
+        class="absolute bottom-5 right-5 text-white cursor-none pointer-events-none"
+      />
+    </div>
+
     <p>{{ blok.intro }}</p>
     <rich-text-renderer :document="blok.body" />
   </div>
 </template>
 
 <script>
+import CarbonImageCopy from '~/components/icons/CarbonImageCopy'
 export default {
+  components: {
+    CarbonImageCopy,
+  },
   props: {
     blok: {
       type: Object,
